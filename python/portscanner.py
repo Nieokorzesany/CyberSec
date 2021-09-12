e83ca730eec92ee4089630bfe7afffa168bdc20c
Piotr Wlazeł <38163577+Nieokorzesany@users.noreply.github.com>
2021-09-12 23:19:15 +0200

    Update portscanner.py

--- python/portscanner.py
+++ python/portscanner.py
@@ -1,33 +1,33 @@
 #!/usr/bin/env python3
 
-import sys
-from datetime import datetime as dt
 import socket
 
-if len(sys.argv) == 2:
-  target = socket.gethostbyname(sys.argv[1]) # translates host to ipv4
-else:
-  print("Invalid number of args")
-  print("Syntax: python3 port_scanner.py [ip/hostname]")
 
-print("Scannning target: " + target)
-print("Time started: " + str(dt.now()))
-print('-' * 50)
 
-try:
-  for port in range(1,65535):
+target = input('Enter the IP address to scan: ')
+
+portrange = input('Enter the port range to scan (es 5-200): ')
+
+lowport = int(portrange.split('-')[0])
+
+highport = int(portrange.split('-')[1])
+
+print('Scanning host ', target, 'from port',lowport, 'to port', highport)
+
+
+for port in range(lowport, highport):
+
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    socket.setdefaulttimeout(0.5)
-    result = s.connect_ex((target, port))
-    if result == 0:
-      print("port {} is open".format(port))
-    s.close 
-except KeyboardInterrupt:
-  print('\nExitting...')
-  sys.exit()
-except socket.gaierror:
-  print("Hostname couldn't be resolved")
-  sys.exit()
-except socket.error:
-  print("Couldn't connect to server")
-  sys.exit()
+
+    status = s.connect_ex((target, port))
+
+    if(status == 0):
+
+        print('*** Port',port,'- OPEN ***')
+
+    else:
+
+        print('Port',port,'- CLOSED')
+
+    s.close()
+
