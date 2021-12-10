354868030b4e47dfb8632939b8487d9b2eae4f17
Piotr Wlazeł <38163577+Nieokorzesany@users.noreply.github.com>
2021-12-10 20:50:51 +0100

    Update bash_base64.rb

--- metasploit/bash_base64.rb
+++ metasploit/bash_base64.rb
@@ -1,1 +1,19 @@
-
+class MetasploitModule < Msf::Encoder
+    Rank = NormalRanking
+  
+  def initialize
+      super{
+        'Name'             => 'Bash Base64 Encoder',
+        'Description'      => %q{
+        Base64 encodes bash scripts..
+        },
+        'Author'           => 'Ethical Hacker :)'
+      }
+  end
+  
+  def encode_block(state, buf)
+    unicode = Rex::Text.to_unicode(buf)
+    base64 = Rex::Text.encode_base64(unicode)
+    cmd = "base64 -d <<< #{base64} | sh"
+    return cmd
+  end
